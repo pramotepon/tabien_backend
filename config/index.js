84bccd571ca59c50caf0384e2a443d0fe91c6c06
Zeekerz <pramote.p.on@gmail.com>
2021-06-18 15:49:25 +0700

    delete Port

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 // export ข้อมูลที่ต้องการส่งออกจาก .env
 module.exports = {
-    PORT: process.env.PORT,
+    // PORT: process.env.PORT,
     DOMAIN: process.env.DOMAIN,
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET
